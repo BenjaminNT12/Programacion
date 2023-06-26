d1b0e721f96894724a0cc8cda044661a43d45cc1
BenjaminNT12 <benjaminnt12@gmail.com>
2023-06-26 17:37:40 -0600

    Update DVL2.py
    lee cualquier archivo

--- DatosDVL/DVL2.py
+++ DatosDVL/DVL2.py
@@ -9,45 +9,49 @@ def leer_archivo(nombre_archivo):
             columnas.append(linea.strip().split(','))
     return columnas
 
-
-nombre_archivo = '/home/nicolas/Github/Programacion/DatosDVL/datosdvl.txt'  # Reemplaza 'archivo.txt' con el nombre de tu archivo
-# nombre_archivo2 = '/home/nicolas/Github/Programacion/DatosDVL/datosdvl.txt'  # Reemplaza 'archivo.txt' con el nombre de tu archivo
+nombre_archivo = '/home/nicolas/Github/Programacion/DatosDVL/dvl7.txt'  # Reemplaza 'archivo.txt' con el nombre de tu archivo
 
 datos = leer_archivo(nombre_archivo)
-# datos2 = leer_archivo(nombre_archivo2)
 
-# columna1 = [columna[0] for columna in datos]
-columna2 = [columna[6] for columna in datos]
-columna3 = [columna[7] for columna in datos]
-columna4 = [columna[8] for columna in datos]
+datosLimpios = []
+
+
+for i in range(len(datos)):
+    if datos[i][0] == '$DVPDL' and len(datos[i][:]) == 10:
+        datosLimpios.append(datos[i])
+  
+columnaX = [columna[6] for columna in datosLimpios]
+columnaY = [columna[7] for columna in datosLimpios]
+columnaZ = [columna[8] for columna in datosLimpios]
 
 carryX = []
 carryY = []
 carryZ = []
 
-for i in range(len(columna2)):
-    # columna1[i] = float(columna1[i])
-    columna2[i] = float(columna2[i])
-    columna3[i] = float(columna3[i])
-    columna4[i] = float(columna4[i])
+for i in range(len(columnaX)):
+    columnaX[i] = float(columnaX[i])
+    columnaY[i] = float(columnaY[i])
+    columnaZ[i] = float(columnaZ[i])
 
     if i == 0:
-        carryX.append(columna2[i]) 
-        carryY.append(columna3[i]) 
-        carryZ.append(columna4[i]) 
+        carryX.append(columnaX[i]) 
+        carryY.append(columnaY[i]) 
+        carryZ.append(columnaX[i]) 
     else:
-        carryX.append(carryX[i-1] + columna2[i])
-        carryY.append(carryY[i-1] + columna3[i])
-        carryZ.append(carryZ[i-1] + columna4[i])
+        carryX.append(carryX[i-1] + columnaX[i])
+        carryY.append(carryY[i-1] + columnaY[i])
+        carryZ.append(carryZ[i-1] + columnaZ[i])
+
 
 ax = plt.figure().add_subplot(projection='3d')
 
 ax.plot(carryX, carryY, carryZ, label='DATOS DVL')
+ax.set_xlim([0, 5])
+ax.set_ylim([-2.5, 2.5])
+ax.set_zlim([0, 5])
 ax.set_xlabel("X Axis")
 ax.set_ylabel("Y Axis")
 ax.set_zlabel("Z Axis")
 ax.legend()
 
 plt.show()
-
-# 
